3ff131d0e63a31f8c3c12e49c6aec63d13506a4e
smetankajakub <smetanka321@gmail.com>
2023-01-30 21:29:59 +0100

    environment api url

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: 'https://60b549eefe923b0017c83df4.mockapi.io/api/v1/'
+
 };
--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiUrl: 'https://60b549eefe923b0017c83df4.mockapi.io/api/v1/'
 };
 
 /*
